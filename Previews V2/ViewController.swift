c6a90ffd9caa2a4b4237f6494aa319598de64ae4
CameronsmithP <70288194+CameronsmithP@users.noreply.github.com>
2022-04-28 15:00:38 -0600

    pushing hopefully nothing old

--- Previews V2/ViewController.swift
+++ Previews V2/ViewController.swift
@@ -29,37 +29,41 @@ class ViewController: UIViewController, ARSessionDelegate {
         func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
             anchors.compactMap { $0 as? ARImageAnchor }.forEach {
                 let anchorEntity = AnchorEntity()
-                let modelEntity = boxAnchor.timsLiberal!
-                anchorEntity.addChild(modelEntity)
-                arView.scene.addAnchor(anchorEntity)
+                let modelEntity = try! Experience.loadScene()
+              //let modelEntity = boxAnchor.timsBall!
+                
+                
+               anchorEntity.addChild(modelEntity)
+               //anchorEntity.addChild(PortalEntity1)
+               arView.scene.addAnchor(anchorEntity)
                 anchorEntity.transform.matrix = $0.transform
                 imageAnchorToEntity[$0] = anchorEntity
             }
         }
 
-        func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+       /* func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
             anchors.compactMap { $0 as? ARImageAnchor }.forEach {
                 let anchorEntity = imageAnchorToEntity[$0]
                 anchorEntity?.transform.matrix = $0.transform
             }
-        }
+        }*/
 
-    func resetTrackingConfig() {
-//Maybe this will make it so it doesnt need to keep resetting the tracking of the image
+   /*func resetTrackingConfig() {
+
         
-       // guard let refImg = ARReferenceImage.referenceImages(inGroupNamed: "AR Resource",
-                        //                                          bundle: nil)
-       // else { return }
+       guard let refImg = ARReferenceImage.referenceImages(inGroupNamed: "AR Resource",
+                                                        bundle: nil)
+       else { return }
 
         let config = ARWorldTrackingConfiguration()
-       // config.detectionImages = refImg
+       config.detectionImages = refImg
         config.maximumNumberOfTrackedImages = 1
 
         let options = [ARSession.RunOptions.removeExistingAnchors,
                        ARSession.RunOptions.resetTracking]
 
         arView.session.run(config, options: ARSession.RunOptions(options))
-    }
+    }*/
         
 }
 
